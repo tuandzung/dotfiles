599b0b307aeca03cbfe6d2e48911d0040744d24c
Ngo Tuan Dzung <zun1903@gmail.com>
2022-06-14 17:22:29 +0700

    feat: fzf color scheme & new gcloud gke login

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -4,3 +4,5 @@ export PATH=$PATH:~/.cargo/bin
 export GOPATH=$(go env GOPATH)
 export PATH=$PATH:$GOPATH/bin
 export PATH="/Users/tuandzung/.local/bin:$PATH"
+
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
