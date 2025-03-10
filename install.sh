7506914604482c822fda2c827d237b9f253160f1
Ngo Tuan Dzung <zun1903@gmail.com>
2025-03-10 17:11:10 +0700

    chore: modify install script

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-# -e: exit on error
-# -u: exit on unset variables
-set -eu
+set -Eeou pipefail
+SETUP_DIR=$(dirname "$(readlink -f "$0")")
+BIN_DIR="$HOME/.local/bin"
+source "${SETUP_DIR}/scripts/logger.sh"
 
 _log_color() {
   color_code="$1"
@@ -36,7 +37,7 @@ _install_chezmoi() {
   if ! chezmoi="$(command -v chezmoi)"; then
     bin_dir="${HOME}/.local/bin"
     chezmoi="${bin_dir}/chezmoi"
-    _log_task "Installing chezmoi to '${chezmoi}'"
+    _notice "Installing chezmoi to '${chezmoi}'"
     if command -v curl >/dev/null; then
       chezmoi_install_script="$(curl -fsSL https://get.chezmoi.io)"
     elif command -v wget >/dev/null; then
@@ -53,7 +54,7 @@ _install_age() {
   if ! age="$(command -v age)"; then
     bin_dir="${HOME}/.local/bin"
     age="${bin_dir}/age"
-    _log_task "Installing age to '${age}'"
+    _notice "Installing age to '${age}'"
     temp=$(mktemp)
     if command -v curl > /dev/null; then
       curl -sSL -o "$temp" https://dl.filippo.io/age/latest?for=linux/amd64
@@ -75,15 +76,18 @@ _install_age() {
 }
 
 _main() {
-  mkdir -p "${HOME}/.local/bin"
-  export PATH="${HOME}/.local/bin:${PATH}"
+  mkdir -p "$BIN_DIR"
+  export PATH="$BIN_DIR:$PATH"
   _install_chezmoi
   _install_age
+
+  _notice "Initialize chezmoi"
+  echo "sourceDir: \"$(readlink -f "$SETUP_DIR")\"" > "$SETUP_DIR"/home/.chezmoi.yaml.tmpl
+  cat "$SETUP_DIR"/.chezmoi.yaml.tmpl >> "$SETUP_DIR"/home/.chezmoi.yaml.tmpl
   # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
   # shellcheck disable=SC2312
-  script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-  set -- init "$@"
+  set -- init -S $SETUP_DIR "$@"
 
   if [ -n "${DOTFILES_ONE_SHOT-}" ]; then
     set -- "$@" --one-shot
@@ -95,7 +99,7 @@ _main() {
     set -- "$@" --debug --verbose --dry-run
   fi
 
-  _log_task "Running 'chezmoi $*'"
+  _notice "Running 'chezmoi $*'"
   # replace current process with chezmoi
   exec "${chezmoi}" "$@"
 }
