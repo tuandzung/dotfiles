5960220136fa59213ea346fa0636146bc7859849
Ngo Tuan Dzung <zun1903@gmail.com>
2025-03-12 22:57:43 +0700

    fix(pkg_installer): update export prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -6,7 +6,7 @@ if command -v sw_vers &> /dev/null; then
   (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ~/.zprofile
   eval "$(/usr/local/bin/brew shellenv)"
   brew update && brew install bash coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep chezmoi age yq
-  echo "export PATH=\"/opt/homebrew/bin:/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/opt/findutils/libexec/gnubin:/opt/homebrew/opt/gnu-tar/libexec/gnubin:/opt/homebrew/opt/gnu-sed/libexec/gnubin:/opt/homebrew/opt/gawk/libexec/gnubin:/opt/homebrew/opt/gnu-indent/libexec/gnubin:/opt/homebrew/opt/gnu-getopt/bin:/opt/homebrew/opt/grep/libexec/gnubin:$PATH\"" >> ~/.zprofile
+  echo "export PATH=\"/usr/local/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:/usr/local/opt/gnu-tar/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/gawk/libexec/gnubin:/usr/local/opt/gnu-indent/libexec/gnubin:/usr/local/opt/gnu-getopt/bin:/usr/local/opt/grep/libexec/gnubin:$PATH\"" >> ~/.zprofile
 elif command -v apt &> /dev/null; then
   sudo apt update && sudo apt install -y git curl gnupg openssh
 elif command -v pacman &> /dev/null; then
