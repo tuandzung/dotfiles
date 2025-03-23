a90d326d06dedddac7405330889af86c6890bc9b
Ngo Tuan Dzung <zun1903@gmail.com>
2025-03-23 21:35:52 +0700

    feat(firefox): added work profile

---
+++ home/.chezmoitemplates/firefox/prefs/work/05-extensions.js
@@ -0,0 +1,53 @@
+user_pref("privacy.userContext.extension", "@testpilot-containers");
+user_pref(
+	"extensions.webextensions.ExtensionStorageIDB.migrated.@testpilot-containers",
+	true,
+);
+user_pref(
+	"extensions.webextensions.ExtensionStorageIDB.migrated.FirefoxColor@mozilla.com",
+	true,
+);
+user_pref(
+	"extensions.webextensions.ExtensionStorageIDB.migrated.addon@darkreader.org",
+	true,
+);
+user_pref(
+	"extensions.webextensions.ExtensionStorageIDB.migrated.capture-this@josh-berry.github.io",
+	true,
+);
+user_pref(
+	"extensions.webextensions.ExtensionStorageIDB.migrated.languagetool-webextension@languagetool.org",
+	true,
+);
+user_pref(
+	"extensions.webextensions.ExtensionStorageIDB.migrated.screenshots@mozilla.org",
+	true,
+);
+user_pref(
+	"extensions.webextensions.ExtensionStorageIDB.migrated.simple-translate@sienori",
+	true,
+);
+user_pref(
+	"extensions.webextensions.ExtensionStorageIDB.migrated.skipredirect@sblask",
+	true,
+);
+user_pref(
+	"extensions.webextensions.ExtensionStorageIDB.migrated.uBlock0@raymondhill.net",
+	true,
+);
+user_pref(
+	"extensions.webextensions.ExtensionStorageIDB.migrated.wappalyzer@crunchlabz.com",
+	true,
+);
+user_pref(
+	"extensions.webextensions.ExtensionStorageIDB.migrated.{3c078156-979c-498b-8990-85f7987dd929}",
+	true,
+);
+user_pref(
+	"extensions.webextensions.ExtensionStorageIDB.migrated.{446900e4-71c2-419f-a6a7-df9c091e268b}",
+	true,
+);
+user_pref(
+	"extensions.webextensions.uuids",
+	'{"formautofill@mozilla.org":"182637bb-d32d-4ad2-b000-f0316d5b7f9f","pictureinpicture@mozilla.org":"1422a698-8228-4065-bb37-72d5dde33d05","screenshots@mozilla.org":"662e7118-b160-43b2-97fa-706f2329acc7","webcompat-reporter@mozilla.org":"48eb5de0-1c9c-4677-8e4e-a190420c2cd1","webcompat@mozilla.org":"2cc4890c-662a-4b0f-b567-2a329b4e7448","default-theme@mozilla.org":"8f749ba7-938c-4f08-8b74-2e4c78600e6d","addons-search-detection@mozilla.com":"67183cea-deab-4774-bcb7-f36605fa7af0","{446900e4-71c2-419f-a6a7-df9c091e268b}":"faf9d124-19b7-4983-b879-ccbe6d1e3b6c","simple-translate@sienori":"e7e3a327-13c3-4d27-82b7-5e257a6d8c12","FirefoxColor@mozilla.com":"557eafc3-5ed9-487c-845e-d41a7f61b2fc","{3c078156-979c-498b-8990-85f7987dd929}":"ebd61cd4-1d0f-44a4-b03a-6b3a93dfe375","hotfix-intermediate-2018@mozilla.com":"c52e680d-9155-463e-9e56-b60c04a2d673","skipredirect@sblask":"75ddc203-7dd1-4156-94f0-3750898bd3ea","wappalyzer@crunchlabz.com":"2c1dd8a6-3614-4586-a2a5-9a87d8dbbc07","capture-this@josh-berry.github.io":"08d22b91-ed32-4b8f-ac00-5d553e540b87","languagetool-webextension@languagetool.org":"a4764c63-17ba-4ed0-80f6-0e093a41a06e","@testpilot-containers":"cb59b2ba-6ff5-41b4-8a9f-a6c123d25bee","uBlock0@raymondhill.net":"354f24a7-d6f2-468a-8ad1-31118cc65de9","addon@darkreader.org":"a35fc141-61fd-4e85-8b33-4f9f7a5294a6"}',
+);
--- home/.chezmoitemplates/firefox/user.js
+++ home/.chezmoitemplates/firefox/user.js
@@ -2,8 +2,8 @@
 {{- $settings := glob (joinPath .chezmoi.sourceDir ".chezmoitemplates/firefox/prefs/*.js*") }}
 {{- if regexMatch ".*personal/user\\.js\\.tmpl" .chezmoi.sourceFile }}
 {{- $settings = concat $settings (glob (joinPath .chezmoi.sourceDir ".chezmoitemplates/firefox/prefs/personal/*.js")) }}
-{{- else if regexMatch ".*enterprise/user\\.js\\.tmpl" .chezmoi.sourceFile }}
-{{- $settings = concat $settings (glob (joinPath .chezmoi.sourceDir ".chezmoitemplates/firefox/prefs/enterprise/*.js")) }}
+{{- else if regexMatch ".*work/user\\.js\\.tmpl" .chezmoi.sourceFile }}
+{{- $settings = concat $settings (glob (joinPath .chezmoi.sourceDir ".chezmoitemplates/firefox/prefs/work/*.js")) }}
 {{- end }}
 {{- $settings = concat $settings (list (joinPath .chezmoi.sourceDir ".chezmoitemplates/firefox/chrome/lepton/user.js")) }}
 {{- range ($settings) }}
