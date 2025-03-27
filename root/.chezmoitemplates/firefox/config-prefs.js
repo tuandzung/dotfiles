a921a47ddc36c51f4a9c389cde34434e1ce368b7
Ngo Tuan Dzung <zun1903@gmail.com>
2025-03-27 20:09:58 +0700

    feat(firefox): use fx-autoconfig for userChrome.js manager

--- root/.chezmoitemplates/firefox/config-prefs.js
+++ root/.chezmoitemplates/firefox/config-prefs.js
@@ -1,3 +1,3 @@
 pref("general.config.obscure_value", 0);
-pref("general.config.filename", "userChrome.js");
+pref("general.config.filename", "config.js");
 pref("general.config.sandbox_enabled", false);
