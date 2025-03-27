166537150db6798caecf34badde6b5491d975d9f
Ngo Tuan Dzung <zun1903@gmail.com>
2025-03-28 00:08:02 +0700

    feat: added some miscellanous zen config

---
+++ home/.chezmoitemplates/zen/prefs/04-misc.js
@@ -0,0 +1,6 @@
+/* Disable welcome screen */
+user_pref("zen.welcome-screen.seen", true);
+/* Color */
+user_pref("zen.theme.accent-color", "#7dc4e4");
+/* Disable new tab button on sidebar */
+user_pref("zen.tabs.show-newtab-vertical", false);
