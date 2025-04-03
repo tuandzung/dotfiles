76b63ad64c1cb7fe8b0b0b7205995eb84187b5b1
Ngo Tuan Dzung <zun1903@gmail.com>
2025-04-04 00:03:59 +0700

    refactor: update prerequisites script on mac

--- prerequisites.sh
+++ prerequisites.sh
@@ -2,11 +2,20 @@
 set -Eeuo pipefail
 
 if command -v sw_vers &> /dev/null; then
+  UNAME_MACHINE="$(/usr/bin/uname -m)"
+  if [[ "${UNAME_MACHINE}" == "arm64" ]]
+  then
+    # On ARM macOS, this script installs to /opt/homebrew only
+    HOMEBREW_PREFIX="/opt/homebrew"
+  else
+    # On Intel macOS, this script installs to /usr/local only
+    HOMEBREW_PREFIX="/usr/local"
+  fi
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash - || :
-  (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ~/.zprofile
-  eval "$(/usr/local/bin/brew shellenv)"
+  (echo; echo 'eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"') >> ~/.zprofile
+  eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
   brew update && brew install bash coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep chezmoi age yq
-  echo "export PATH=\"/usr/local/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:/usr/local/opt/gnu-tar/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/gawk/libexec/gnubin:/usr/local/opt/gnu-indent/libexec/gnubin:/usr/local/opt/gnu-getopt/bin:/usr/local/opt/grep/libexec/gnubin:$PATH\"" >> ~/.zprofile
+  echo "export PATH=\"$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$HOMEBREW_PREFIX/opt/findutils/libexec/gnubin:$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin:$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin:$HOMEBREW_PREFIX/opt/gawk/libexec/gnubin:$HOMEBREW_PREFIX/opt/gnu-indent/libexec/gnubin:$HOMEBREW_PREFIX/opt/gnu-getopt/bin:$HOMEBREW_PREFIX/opt/grep/libexec/gnubin:$PATH\"" >> ~/.zprofile
 elif command -v apt &> /dev/null; then
   sudo apt update && sudo apt install -y git curl gnupg openssh
 elif command -v pacman &> /dev/null; then
