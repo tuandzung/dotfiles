add442eaa595a69de65b74e49298ee72f8d3a8be
Ngo Tuan Dzung <mail@tuandzung.me>
2025-07-03 09:19:54 +0700

    fix(hyprland): fix xdg-desktop-portal autostart script

--- home/dot_config/hypr/hyprland/scripts/executable_xdg-portal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-killall xdg-desktop-portal-wlr
-killall xdg-desktop-portal-gnome
-killall xdg-desktop-portal-hyprland
-killall xdg-desktop-portal
-sleep 1
-/usr/libexec/xdg-desktop-portal-hyprland &
-sleep 2
-/usr/libexec/xdg-desktop-portal &
