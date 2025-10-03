a56ae62bf6168ba83fec5cc4229eacd985b1c111
Ngo Tuan Dzung <mail@tuandzung.me>
2025-10-03 15:32:01 +0700

    feat(zen): updated zen profiles and preferences

--- home/.chezmoitemplates/zen/prefs/04-misc.js
+++ home/.chezmoitemplates/zen/prefs/04-misc.js
@@ -4,3 +4,7 @@ user_pref("zen.welcome-screen.seen", true);
 user_pref("zen.theme.accent-color", "#7dc4e4");
 /* Disable new tab button on sidebar */
 user_pref("zen.tabs.show-newtab-vertical", false);
+/* Multiple toolbars */
+user_pref("zen.view.use-single-toolbar", false);
+/* Enable container-specific essentials */
+user_pref("zen.workspaces.container-specific-essentials-enabled", true);
