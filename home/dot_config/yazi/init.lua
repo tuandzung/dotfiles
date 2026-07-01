8dad83421b47c86b41f6c8bd2fa07d1ea3870b6e
Ngo Tuan Dzung <mail@tuandzung.me>
2026-07-01 17:00:10 +0700

    feat(yazi): added extra plugins

--- home/dot_config/yazi/init.lua
+++ home/dot_config/yazi/init.lua
@@ -1,5 +1,13 @@
 require("git"):setup()
+
 require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
+})
+
+-- DuckDB plugin configuration
+require("duckdb"):setup()
+
+require("smart-enter"):setup({
+	open_multi = true,
 })
